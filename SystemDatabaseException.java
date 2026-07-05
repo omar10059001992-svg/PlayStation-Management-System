ebb4429cbe77f86d630792c098499ac97beffb3e
omar10059001992-svg <omar10059001992@gmail.com>
2026-07-06 02:29:34 +0300

    Document SystemDatabaseException class logic

--- SystemDatabaseException.java
+++ SystemDatabaseException.java
@@ -1,7 +1,7 @@
 
 package gaming;
 
-
+// Custom exception for handling database retrieval and connection errors
 
 public class SystemDatabaseException extends Exception {
     public SystemDatabaseException(String message) {
